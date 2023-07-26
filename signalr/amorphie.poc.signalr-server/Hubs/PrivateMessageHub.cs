a14e37ca7bd915f71c8124d3508d49c56d1623d2
mt <mehmet.tosun@outlook.com>
2023-07-26 14:33:31 +0300

    Added Sample for countly Flutter and Web SDK

--- signalr/amorphie.poc.signalr-server/Hubs/PrivateMessageHub.cs
+++ signalr/amorphie.poc.signalr-server/Hubs/PrivateMessageHub.cs
@@ -2,10 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using amorphie.poc.signalr_server.Model;
+using Microsoft.AspNetCore.SignalR;
 
 namespace amorphie.poc.signalr_server.Hubs;
 
-    public class PrivateMessageHub
+    public class PrivateMessageHub: Hub
     {
-        
+        public async Task SendMessage(HubMessage message)
+        {
+            
+        }
+        /*
+        public async Task SendMessageToClient(HubMessage hubMessage) =>
+            await Clients.Client(connectionId).SendAsync("SendMessage", data);
+
+        public async Task SendMessageToUser(string data, string userId) =>
+            await Clients.User(userId).SendAsync("SendMessage", data);
+*/
+
     }
+
